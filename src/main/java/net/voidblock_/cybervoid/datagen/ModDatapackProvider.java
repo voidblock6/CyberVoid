2b506bea2bab78abf834ad5f11913acbcb9386b8
GamePC <rkoolma@icloud.com>
2025-11-07 19:44:02 +0100

    worldgen setup

---
+++ src/main/java/net/voidblock_/cybervoid/datagen/ModDatapackProvider.java
@@ -0,0 +1,27 @@
+package net.voidblock_.cybervoid.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import net.voidblock_.cybervoid.CyberVoid;
+import net.voidblock_.cybervoid.worldgen.ModBiomeModifiers;
+import net.voidblock_.cybervoid.worldgen.ModConfiguredFeatures;
+import net.voidblock_.cybervoid.worldgen.ModPlacedFeatures;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(CyberVoid.MOD_ID));
+    }
+}
---
+++ src/main/java/net/voidblock_/cybervoid/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,24 @@
+package net.voidblock_.cybervoid.worldgen;
+
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import net.voidblock_.cybervoid.CyberVoid;
+
+public class ModBiomeModifiers {
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        // CF -> PF -> BM
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(CyberVoid.MOD_ID, name));
+    }
+}
